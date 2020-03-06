90d06f5be341ea1e14bbc646480ad8eaa95b6a7f
Taylor Wood <taylorwood39@gmail.com>
2020-03-05 20:45:59 -0600

    Add Question 3 Progress

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ while (!gameQuestion || gameQuestion !== gameDetails[0].game) {
   gameQuestion = String(prompt("What game are you playing?"));
 }
 // Reward Proper Response
-let confirmation = alert("Halo Reach was selected!");
+let confirmation = alert(`${gameQuestion} was selected!`);
 
 // Question 2
 // Alert user of limited selection
@@ -30,4 +30,23 @@ while (!playlistQuestion || playlistQuestion !== gameDetails[1].playlist) {
   playlistQuestion = String(prompt("What playlist are you playing?"));
 }
 // Reward Proper Response
-confirmation = alert("Team Hardcore was selected!");
+confirmation = alert(`${playlistQuestion} was selected!`);
+
+// Question 3
+// Alert user of limited selections
+let modeAlert = alert(
+  "Game modes available include: Team Slayer, Capture the Flag, and King of the Hill."
+);
+let modeQuestion = String(prompt("Which game mode are you playing?"));
+for (let counter = 0; counter <= gameDetails[2].mode.length - 1; counter++) {
+  let index = 0;
+  while (!modeQuestion || modeQuestion !== gameDetails[2].mode[index]) {
+    // modeAlert = alert(
+    //   "Please pick one of the following: Team Slayer, Capture the Flag, or King of the Hill"
+    // );
+    modeQuestion = String(prompt("Which game mode are you playing?"));
+    index++;
+  }
+}
+// Reward Proper Response
+confirmation = alert(`${modeQuestion} was selected!`);
